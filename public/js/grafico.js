6cbddd4b34d267798b1a706245c705a56d6966a4
davidruizdiaz <adavidruizdiaz@gmail.com>
2024-04-12 08:23:54 -0400

    up: incluido hora de fhasta en getData

--- public/js/grafico.js
+++ public/js/grafico.js
@@ -15,68 +15,68 @@ let conf = {};
 
 btn.addEventListener('click', procesar);
 
-async function procesar(){
+async function procesar() {
   await validaFechas();
-  if(!conf.fechasValidas) return;
+  if (!conf.fechasValidas) return;
   await getData();
   await loadData();
   console.log('kkkkjsjs');
 }
 
-async function validaFechas(){
+async function validaFechas() {
   const fdesde = dayjs(inDesde.value);
   const fhasta = dayjs(inHasta.value);
-  if(fdesde.isAfter(fhasta)){
-    inDesdeInv.textContent='Fecha inválida';
-    return {...conf, fechasValidas: false}
+  if (fdesde.isAfter(fhasta)) {
+    inDesdeInv.textContent = 'Fecha inválida';
+    return { ...conf, fechasValidas: false }
   } else {
-    inDesdeInv.textContent='';
+    inDesdeInv.textContent = '';
   }
   const ydiff = fhasta.diff(fdesde, 'y');
   const mdiff = fhasta.diff(fdesde, 'M');
-  if(ydiff > 1){
-    conf = {...conf, fechasValidas: true, vista: 'AÑO', fdesde, fhasta}
-    return ;
+  if (ydiff > 1) {
+    conf = { ...conf, fechasValidas: true, vista: 'AÑO', fdesde, fhasta }
+    return;
   }
-  if(mdiff > 1){
-    conf = {...conf, fechasValidas: true, vista: 'MES', fdesde, fhasta}
-    return ;
+  if (mdiff > 1) {
+    conf = { ...conf, fechasValidas: true, vista: 'MES', fdesde, fhasta }
+    return;
   }
-  conf = {...conf, fechasValidas: true, vista: 'DIA', fdesde, fhasta}
-  return ;
+  conf = { ...conf, fechasValidas: true, vista: 'DIA', fdesde, fhasta }
+  return;
 }
 
-async function getData(){
+async function getData() {
   const fdesde = conf.fdesde.format('YYYY-MM-DD');
   const fhasta = conf.fhasta.format('YYYY-MM-DD');
   try {
-    const res = await fetch( URLBASE+ENDPOINT_GRAFICO,{
+    const res = await fetch(URLBASE + ENDPOINT_GRAFICO, {
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({desde: fdesde, hasta: fhasta, vista: conf.vista})
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ desde: fdesde, hasta: `${fhasta} 23:59:59`, vista: conf.vista })
     });
     const data = await res.json();
-    if(!data.ok){
+    if (!data.ok) {
       throw new Error('Algo salió mal');
     }
-    conf = {...conf, data:[...data.data]};
-    return ;
+    conf = { ...conf, data: [...data.data] };
+    return;
   } catch (err) {
     console.error(err);
   }
 }
 
-async function loadData(){
-  const {data, vista} = conf;
+async function loadData() {
+  const { data, vista } = conf;
   chart.data.labels = data.map(i => i.label);
   chart.data.datasets[0] = {
-    data: data.map(i=>i.cant),
+    data: data.map(i => i.cant),
     label: 'CANTIDAD DE VEHÍCULOS POR ' + vista
   };
   chart.update();
 }
 
-const chart = new Chart(ctx,{
+const chart = new Chart(ctx, {
   type: 'line',
 });
 
